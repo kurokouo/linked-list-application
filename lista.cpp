50193314161bb631dbc63ef1140b42da9f603e83
ReneGarmatter <renegarmater@gmail.com>
2025-06-04 16:54:29 -0300

    done

--- lista.cpp
+++ lista.cpp
@@ -21,7 +21,7 @@ struct Node {
   Node *next;
 };
 
-Node *build(int id, std::string descript, int priority) {
+Node *build(int id, std::string descript, int priority) { // single node builder
   Node *newNode = new Node();
   newNode->tasks.ID = id;
   newNode->tasks.description = descript;
@@ -31,7 +31,7 @@ Node *build(int id, std::string descript, int priority) {
   return newNode;
 }
 
-void insert(Node *&head, int id, std::string descript, int priority) {
+void insert(Node *&head, int id, std::string descript, int priority) { // inserts a new node on the list
   Node *newnode = build(id, descript, priority);
   if (head ==
       nullptr) { // if the current node is the first it is null, so just use it
@@ -49,7 +49,7 @@ void insert(Node *&head, int id, std::string descript, int priority) {
   newnode->prev = current;
 }
 
-Node *sortnodesID(Node *start) {
+Node *sortnodesID(Node *start) { // sorts nodes based on priority
   if (start == nullptr) {
     std::cout << "there are no elements to exhibit\n";
     return start;
@@ -78,7 +78,7 @@ Node *sortnodesID(Node *start) {
   }
   return correct;
 }
-Node *findID(Node *start, int id) {
+Node *findID(Node *start, int id) { // returns a node based on given id
   Node *current = start;
   while (current != nullptr) {
     if (current->tasks.ID == id) {
@@ -99,7 +99,7 @@ void printID(Node *start, int id) {
             << "task description: " << print->tasks.description << "\n"
             << "task priority: " << print->tasks.priority;
 }
-void removeID(Node *&start, int id) {
+void removeID(Node *&start, int id) { // removes a node based on its id
   Node *dead = findID(start, id); // dead because its going to be deleted
   if (dead == nullptr) {
     std::cout << "task was not found :(";
@@ -123,7 +123,7 @@ void removeID(Node *&start, int id) {
   }
 }
 
-void printList(Node *start) {
+void printList(Node *start) { // prints the entire sorted list
   Node *current = sortnodesID(start);
   while (current != nullptr) { // traverse all the nodes to print them all
     std::cout << std::string(30, '=') << std::endl;
@@ -137,7 +137,7 @@ void printList(Node *start) {
   std::cout << std::string(30, '=') << std::endl;
 }
 
-void removePriority(Node *&start, int priority) {
+void removePriority(Node *&start, int priority) { // removes nodes based on given priority
   Node *current = start;
 
   while (current != nullptr) {
@@ -149,7 +149,7 @@ void removePriority(Node *&start, int priority) {
   }
 }
 
-bool checkID(Node *&head, int id) {
+bool checkID(Node *&head, int id) { // checks if the id is already taken
   if (findID(head, id) != nullptr) {
     return true;
   } else {
@@ -157,7 +157,7 @@ bool checkID(Node *&head, int id) {
   }
 }
 
-bool checkInterval(int num) {
+bool checkInterval(int num) { // checks if a int x is 1 < x < 3
   if (num < 1 || num > 3) {
     return true;
   } else {
@@ -166,7 +166,7 @@ bool checkInterval(int num) {
 }
 
 int main() {
-
+  // welcoming ascii art text
   std::cout << "  _       _____ ______             _____ _                 _   "
                "    _             \n"
             << " | |     / ____|  ____|           / ____(_)               | |  "
@@ -182,20 +182,25 @@ int main() {
             << "                                                               "
                "                  \n";
 
+
   Node *head = nullptr;
   std::cout << menu;
   std::string command = "";
-  while (true) {
+
+
+  while (true) { // command loop
     std::cout << "Enter command(menu to see commands): ";
     std::cin >> command;
+
     if (command == "exit") {
       std::cout << "Exiting the program\n";
       break;
-    } else if (command == "insert") {
+    } else if (command == "insert") { // insert command
       std::cout << "digit the ammount of elements you want to add \n";
       int num = 0;
       std::cin >> num;
 
+      // node insertion loop (receives each new node and stores them)
       while (num--) {
         int id;
         std::string description;
@@ -206,11 +211,13 @@ int main() {
         std::cin >> id >> priority;
         std::getline(std::cin, description);
 
+        // reruns the input in case the id is already taken
         while (checkID(head, id)) {
           std::cout << "id is already being used, digit another one:\n";
           std::cin >> id;
         }
 
+        //reruns the input in case the priority interval is < 1 or > 3
         while(checkInterval(priority)) {
           std::cout << "please enter a priority from 1 to 3 to task with id: "
                     << id << std::endl;
@@ -219,12 +226,12 @@ int main() {
 
         insert(head, id, description, priority);
       }
-    } else if (command == "deleteID") {
+    } else if (command == "deleteID") { // delete command
       std::cout << "digit the id you want to delete" << std::endl;
       int id;
       std::cin >> id;
       removeID(*&head, id);
-    } else if (command == "search") {
+    } else if (command == "search") { // search command
       std::cout << "digit the id you want to insert" << std::endl;
       int id;
       std::cin >> id;
@@ -233,22 +240,22 @@ int main() {
 
       std::cout << target->tasks.ID << " | " << target->tasks.description
                 << " | " << target->tasks.priority << " | " << std::endl;
-    } else if (command == "DeletePrio") {
+    } else if (command == "DeletePrio") { // delete based on priority command
       std::cout << "digit the priority you want to delete" << std::endl;
       int priority;
       std::cin >> priority;
 
       removePriority(*&head, priority);
-    } else if (command == "exhibit") {
+    } else if (command == "exhibit") { // exhibit command 
       printList(*&head);
 
-    } else if (command == "menu") {
+    } else if (command == "menu") { // help command
       std::cout << menu;
     } else {
-      std::cout << "Invalid command! Please try again.\n";
+      std::cout << "Invalid command! Please try again.\n"; // invalid command error
     }
     for (int i = 0; i < 3; i++) {
-      std::cout << std::endl;
+      std::cout << std::endl; // command spacing
     }
   }
 }
